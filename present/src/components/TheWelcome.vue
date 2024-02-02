37e298f9dcbc2bb3a9117e04c16c7251e2dc51b3
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-02-02 10:15:58 -0500

    rw

---
+++ present/src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 v-if="answer"> Correct</h1>
+    <h1 v-else>Incorrect</h1>
+    <h3 v-for="animal in animals" :key="animal">{{  animal }}</h3>
+  </div>
+</template>
+
+<script setup>
+const answer = true;
+const animals = ["Ape", "Zebra", "Katie"];
+</script>
+
+<style scoped>
+</style>
