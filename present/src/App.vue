41d0394fe34b3a8506af6047edf0a145af20a909
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-02-12 10:14:20 -0500

    w4t

--- present/src/App.vue
+++ present/src/App.vue
@@ -3,9 +3,26 @@
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper"> </div>
+    <div class="wrapper">
 
-  }
-</header>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+
+
+</style>
